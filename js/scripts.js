d61cfc149fc8fe6d4351b4fa39eaf8162da159df
Yulia Shidlovskaya <sjullieb@gmail.com>
2019-01-25 14:26:04 -0800

    add textbox for user's name, add name to the output

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,44 @@
 
-var beepBoop = function(number){
+var beepBoop = function(number, name){
   var stringNumber = "";
   stringNumber = number.toString();
 
   if (number % 3 === 0){
-    return("'I'm sorry...'");
+    return("'I'm sorry, " + name + ". I'm afraid I can't do that.'");
   } else if (stringNumber.includes("1")) {
-    return("'Boop!''");
+    return("'Boop!'");
   } else if (stringNumber.includes("0")) {
-    return("'Beep!''");
+    return("'Beep!'");
   } else {
     return(stringNumber);
   }
-
 };
 
 $(document).ready(function(){
-  $("#number").focus();
+  $("#name").focus();
 
   $("form").submit(function(event){
     event.preventDefault();
 
+    var userName = $("#name").val();
     var inputNumber = parseInt($("#number").val());
     var outputText = [];
 
     for(var index = 0; index <= inputNumber; index++){
-      outputText.push(beepBoop(index));
+      outputText.push(beepBoop(index, userName));
     }
+
+    $(".name").text(userName);
+    $("#result").text(outputText.join(", "));
+
     $(".input").hide();
     $(".result").show();
-    $("#result").text(outputText.join(", "));
   });
 
   $("#again").click(function(){
     $(".input").show();
     $(".result").hide();
-    $("#number").text("");
+    $("#number").val("");
     $("#number").focus();
   });
 });
